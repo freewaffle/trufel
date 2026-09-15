4750ba3d7dec9fbaa0d53430673d08c921e1c795
freewaffle <curtis.cmux@ya.ru>
2026-09-15 15:34:16 +0400

    compact `Instruction` enum

--- src/compiler.rs
+++ src/compiler.rs
@@ -118,121 +118,32 @@ struct Command {
 #[repr(u8)]
 #[derive(Debug)]
 enum Instruction {
-    Mov {
-        r0: u8,
-        c0: u16
-    },
-
-    Add {
-        r0: u8,
-        r1: u8,
-        r2: u8,
-    },
-    Sub {
-        r0: u8,
-        r1: u8,
-        r2: u8,
-    },
-    Mul {
-        r0: u8,
-        r1: u8,
-        r2: u8,
-    },
-    Div {
-        r0: u8,
-        r1: u8,
-        r2: u8,
-    },
-    Mod {
-        r0: u8,
-        r1: u8,
-        r2: u8,
-    },
-
-    And {
-        r0: u8,
-        r1: u8,
-        r2: u8,
-    },
-    Or {
-        r0: u8,
-        r1: u8,
-        r2: u8,
-    },
-    Xor {
-        r0: u8,
-        r1: u8,
-        r2: u8,
-    },
-    Not {
-        r0: u8,
-        r1: u8,
-    },
-
-    Shl {
-        r0: u8,
-        r1: u8,
-        r2: u8,
-    },
-    Shr {
-        r0: u8,
-        r1: u8,
-        r2: u8,
-    },
-
-    Eq {
-        r0: u8,
-        r1: u8,
-        r2: u8,
-    },
-    Neq {
-        r0: u8,
-        r1: u8,
-        r2: u8,
-    },
-    Lt {
-        r0: u8,
-        r1: u8,
-        r2: u8,
-    },
-    Lte {
-        r0: u8,
-        r1: u8,
-        r2: u8,
-    },
-    Gt {
-        r0: u8,
-        r1: u8,
-        r2: u8,
-    },
-    Gte {
-        r0: u8,
-        r1: u8,
-        r2: u8,
-    },
-
-    Jmp {
-        pos: u16
-    },
+    Mov { r0: u8, c0: u16 },
+    Add { r0: u8, r1: u8, r2: u8 },
+    Sub { r0: u8, r1: u8, r2: u8 },
+    Mul { r0: u8, r1: u8, r2: u8 },
+    Div { r0: u8, r1: u8, r2: u8 },
+    Mod { r0: u8, r1: u8, r2: u8 },
+    And { r0: u8, r1: u8, r2: u8 },
+    Or  { r0: u8, r1: u8, r2: u8 },
+    Xor { r0: u8, r1: u8, r2: u8 },
+    Not { r0: u8, r1: u8 },
+    Shl { r0: u8, r1: u8, r2: u8 },
+    Shr { r0: u8, r1: u8, r2: u8 },
+    Eq  { r0: u8, r1: u8, r2: u8 },
+    Neq { r0: u8, r1: u8, r2: u8 },
+    Lt  { r0: u8, r1: u8, r2: u8 },
+    Lte { r0: u8, r1: u8, r2: u8 },
+    Gt  { r0: u8, r1: u8, r2: u8 },
+    Gte { r0: u8, r1: u8, r2: u8 },
+    Jmp { pos: u16 },
     Cmp,
-
-    Call {
-        pos: u16
-        // args?
-    },
+    Call { pos: u16 },
     Ret,
-
-    Store {
-        r0: u8,
-        r1: u8,
-    },
-    Fetch {
-        r0: u8,
-        r1: u8,
-    },
-
+    Store { r0: u8, r1: u8 },
+    Fetch { r0: u8, r1: u8 },
     Int,
-    Halt
+    Halt,
 }
 
 type Bytecode = Vec<Instruction>;
